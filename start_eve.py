6cdd54288f34297ad1e7ff1c90463c74649442be
Erik de Jonge <erik@a8.nl>
2015-04-23 15:35:54 +0200

    rabshakeh Thursday 23 April 2015 (week:16 day:113), 15:35:52

--- start_eve.py
+++ start_eve.py
@@ -10,18 +10,24 @@ def main():
     """
     main
     """
-    machine_eve = grep(machine("ls"), "eve").strip()
+    x = os.popen("ps aux | grep vbox | grep -v grep").read().strip()
 
-    if "Stopped" in machine_eve or "Saved" in machine_eve:
-        machine("start", "eve")
-        print(1)
-    elif "Running" in machine_eve:
-        print(0)
+    if len(x) < 300 or len(x) > 400:
+        print("check eve", len(x))
+        machine_eve = grep(machine("ls"), "eve").strip()
+
+        if "Stopped" in machine_eve or "Saved" in machine_eve:
+            machine("start", "eve")
+            print(1)
+        elif "Running" in machine_eve:
+            print(0)
+        else:
+            for i in range(10):
+                machine_eve = machine_eve.replace("   ", "  ")
+
+            print("\033[31mError:", machine_eve, "\033[0m")
     else:
-        for i in range(10):
-            machine_eve = machine_eve.replace("   ", "  ")
-
-        print("\033[31mError:", machine_eve, "\033[0m")
+        print("0")
 
 
 if __name__ == "__main__":
