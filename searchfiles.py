ddce4a1f79456151dae45ec0ae5bbdf7573608f8
Erik de Jonge <erik@a8.nl>
2015-06-16 10:30:05 +0200

    Bill Cosby: Humor is mankinds greatest blessing. Tuesday 16 June 2015 (week:24 day:167), 10:30:03

--- searchfiles.py
+++ searchfiles.py
@@ -7,6 +7,8 @@ Usage:
 
 Options:
   -h --help     Show this screen.
+  -r --reset    Reset searchcache
+  -c --clean    Clean searchcache
 
 author  : rabshakeh (erik@a8.nl)
 project : devenv
@@ -16,7 +18,7 @@ import os
 import pickle
 
 from arguments import Arguments
-from consoleprinter import get_safe_string
+from consoleprinter import get_safe_string, print_stdout
 
 
 class IArguments(Arguments):
@@ -30,6 +32,8 @@ class IArguments(Arguments):
         self.extension = ""
         self.searchfolder = ""
         self.help = False
+        self.reset = False
+        self.clean = False
         self.query = ""
         super().__init__(doc)
 
@@ -46,6 +50,7 @@ def main():
     main
     """
     arguments = IArguments(__doc__)
+
     cachefolder = os.path.join(os.path.expanduser("~"), ".searchfiles")
 
     if os.path.exists(cachefolder) and os.path.isfile(cachefolder):
@@ -53,18 +58,41 @@ def main():
 
     if not os.path.exists(cachefolder):
         os.makedirs(cachefolder, exist_ok=True)
-
+    if arguments.clean is True:
+        for filepath in os.listdir(cachefolder):
+            print(filepath)
+        return
     cachefilename = os.path.join(cachefolder, arguments.get_cachename())
     searchstruct = {}
 
-    if os.path.exists(cachefilename):
-        searchstruct = pickle.load(open(cachefilename))
+    if arguments.reset is False and os.path.exists(cachefilename):
 
-    for root, folders, files in os.walk(arguments.searchfolder):
-        for posfile in files:
-            possible_test_file_path = os.path.join(root, posfile)
-            if possible_test_file_path.endswith(arguments.extension):
-                searchstruct[possible_test_file_path] = open(searchstruct).read()
+        searchstruct = pickle.load(open(cachefilename, "rb"))
+        print("\033[34mloading search",len(searchstruct),"\033[0m")
+    else:
+        print("\033[34mbuilding search\033[0m")
+
+        for root, folders, files in os.walk(arguments.searchfolder):
+            if len(searchstruct) % 100 == 0:
+
+            for posfile in files:
+                possible_test_file_path = os.path.join(root, posfile)
+                #print(arguments.extension.strip(), possible_test_file_path)
+                if arguments.extension.strip()=="all" or possible_test_file_path.lower().endswith(arguments.extension.lower()):
+                    try:
+                        searchstruct[possible_test_file_path] = open(possible_test_file_path).read()
+                    except UnicodeDecodeError as ex:
+                        searchstruct[possible_test_file_path] = str(ex)
+                    except FileNotFoundError as ex:
+                        searchstruct[possible_test_file_path] = str(ex)
+                    except OSError as ex:
+                        searchstruct[possible_test_file_path] = str(ex)
+        pickle.dump(searchstruct, open(cachefilename, "wb"))
+
+    for filepath in searchstruct:
+        if arguments.query.lower() in searchstruct[filepath].lower():
+            print("\033[33m" + filepath + "\033[0m")
+            print("\033[37m" + str(os.popen("cat -n '" + filepath + "' | tr '[:upper:]' '[:lower:]' | grep " + arguments.query.lower()).read()) + "\033[0m")
 
 
 if __name__ == "__main__":
--- sortalias.py
+++ sortalias.py
@@ -1,9 +1,10 @@
+#!/usr/bin/env python3
 # coding=utf-8
 """
-Prints aliasses where query is in alias or implementation
+Reformats aliasses
 
 Usage:
-  sortalias [options] <query>
+  sortalias
 
 Options:
   -h --help     Show this screen.
@@ -12,17 +13,33 @@ author  : rabshakeh (erik@a8.nl)
 project : devenv
 created : 15-06-15 / 15:45
 """
-from arguments import Arguments
+import sys
 
 
 def main():
     """
     main
     """
-    arguments = Arguments(__doc__)
-    print(arguments)
-    #"alia"s | grep $1
-    #['@!@'+x for x in lines]" | puf "['\033[33m'+x.split('=')[0].lstrip('alia'+'s').strip() +':\033[30m\n'+x.split('=')[1].strip('\'')+'\033[0m\n' for x in lines]" | puf "[x for x in lines if x]" | puf "'\\x1b[33m'+''.join(['  '+x.strip()+'\n' for x in '  '.join(lines).strip().split(';') if x]).strip().replace(':', ':\n').strip().replace('@!@', '\n\n').replace('  \\x1b[', '\\x1b[').replace('\n\n\n', '\n\n').replace('\n\x1b[0m\x1b[33m\n\n', '\x1b[0m\x1b[33m\n\n').lstrip('\x1b[33m\n\n')"
+    lines = []
+
+    for line in sys.stdin:
+        result = '\033[33m' + line.split('=')[0].strip().replace(':', '').strip()
+        implementation = line.split('=')[1].strip().strip('\'')
+        implementation = implementation.replace(";", "\n")
+
+        if not implementation.endswith("\n"):
+            implementation += "\n"
+
+        implist = []
+
+        for impitem in implementation.split("\n"):
+            implist.append(impitem.strip())
+
+        implementation = "\n".join(implist)
+        result += '\033[30m\n' + implementation + '\033[0m'
+        result = result.replace("alias ", "")
+        print(result)
+
 
 if __name__ == "__main__":
     main()
