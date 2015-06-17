ec4847f83ea2d1bd314dd2db8bfa6b15be6977a3
Erik de Jonge <erik@a8.nl>
2015-06-17 15:09:29 +0200

    Martin Filler: If co-operation, is thus the lifeblood of science and technology, it is similarly  vital to society as a whole. Wednesday 17 June 2015 (week:24 day:168), 15:09:28

---
+++ sortals.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+# coding=utf-8
+"""
+Reformats aliasses
+
+Usage:
+  sortalias
+
+Options:
+  -h --help     Show this screen.
+
+author  : rabshakeh (erik@a8.nl)
+project : devenv
+created : 15-06-15 / 15:45
+"""
+import sys
+
+
+def main():
+    """
+    main
+    """
+    lines = []
+
+    for line in sys.stdin:
+        result = '\033[33m' + line.split('=')[0].strip().replace(':', '').strip()
+        implementation = line.split('=')[1].strip().strip('\'')
+        implementation = implementation.replace(";", "\n")
+
+        if not implementation.endswith("\n"):
+            implementation += "\n"
+
+        implist = []
+
+        for impitem in implementation.split("\n"):
+            implist.append(impitem.strip())
+
+        implementation = "\n".join(implist)
+        result += '\033[30m\n' + implementation + '\033[0m'
+        result = result.replace("alias ", "")
+        print(result)
+
+
+if __name__ == "__main__":
+    main()
