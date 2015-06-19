a698a0fde4c3897c7ddf7817e2a8710a572f7ae6
Erik de Jonge <erik@a8.nl>
2015-06-19 16:14:53 +0200

    Adam Sandler: A mans growth is seen in the successive choirs of his friends. Friday 19 June 2015 (week:24 day:170), 16:14:50

--- searchfiles.py
+++ searchfiles.py
@@ -1,6 +1,8 @@
+#!/usr/bin/env python3
 # coding=utf-8
 """
-Search directory for a certain string
+
+Search directory for a certain string (extension [all] is all files)
 
 Usage:
   searchfiles [options] <searchfolder> <extension> <query>
@@ -9,6 +11,7 @@ Options:
   -h --help     Show this screen.
   -r --reset    Reset searchcache
   -c --clean    Clean searchcache
+  --hidden      Hidden only
 
 author  : rabshakeh (erik@a8.nl)
 project : devenv
@@ -18,7 +21,7 @@ import os
 import pickle
 
 from arguments import Arguments
-from consoleprinter import get_safe_string, print_stdout
+from consoleprinter import get_safe_string
 
 
 class IArguments(Arguments):
@@ -34,6 +37,7 @@ class IArguments(Arguments):
         self.help = False
         self.reset = False
         self.clean = False
+        self.hidden = False
         self.query = ""
         super().__init__(doc)
 
@@ -41,53 +45,85 @@ class IArguments(Arguments):
         """
         get_cachename
         """
-        cachename = self.searchfolder + "_" + self.extension
+        cachename = self.searchfolder + "_" + self.extension + str(self.hidden)
         return get_safe_string(cachename).lower()
+
+
+def dosearch(arguments, cachefilename, searchstruct):
+    """
+    @type arguments: IArguments
+    @type cachefilename: str
+    @type searchstruct: dict
+    @return: None
+    """
+    if arguments.reset is False and os.path.exists(cachefilename):
+        searchstruct = pickle.load(open(cachefilename, "rb"))
+        print("\033[34mloading search", len(searchstruct), "\033[0m")
+    else:
+        print("\033[34mbuilding search\033[0m")
+
+        for root, _, files in os.walk(arguments.searchfolder):
+            for posfile in files:
+                possible_test_file_path = os.path.join(root, posfile)
+
+                if arguments.extension.strip() == "all" or possible_test_file_path.lower().endswith(arguments.extension.lower()):
+                    if arguments.hidden is True and not (os.path.basename(root).startswith(".") or os.path.basename(possible_test_file_path.lower()).startswith(".")):
+                        pass
+                    else:
+                        if arguments.hidden is False or (not strinstr([root, possible_test_file_path], [".git", ".dep", ".npm", ".travis", ".bower", "node_modules", ".hg", ".idea", "__MACOSX",  ".fetch.json", ".DS_Store"])):
+                            print(possible_test_file_path)
+                            try:
+                                searchstruct[possible_test_file_path]=open(possible_test_file_path).read()
+                            except UnicodeDecodeError as ex:
+                                searchstruct[possible_test_file_path]=str(ex)
+                            except FileNotFoundError as ex:
+                                searchstruct[possible_test_file_path]=str(ex)
+                            except OSError as ex:
+                                searchstruct[possible_test_file_path]=str(ex)
+
+        pickle.dump(searchstruct, open(cachefilename, "wb"))
+
+    return searchstruct
+
+
+def strinstr(stringlist, itemlist):
+    """
+    @type stringlist: list
+    @type itemlist: list
+    @return: None
+    """
+    for teststr in stringlist:
+        for item in itemlist:
+            if item in teststr:
+                return True
+
+    return False
 
 
 def main():
     """
     main
     """
-    arguments = IArguments(__doc__)
-
-    cachefolder = os.path.join(os.path.expanduser("~"), ".searchfiles")
+    arguments=IArguments(__doc__)
+    print(arguments)
+    cachefolder=os.path.join(os.path.expanduser("~"), ".searchfiles")
 
     if os.path.exists(cachefolder) and os.path.isfile(cachefolder):
         raise RuntimeError("cachefolder is file: " + str(cachefolder))
 
     if not os.path.exists(cachefolder):
         os.makedirs(cachefolder, exist_ok=True)
+
     if arguments.clean is True:
         for filepath in os.listdir(cachefolder):
-            print(filepath)
+            print(os.path.join(cachefolder, filepath))
+            os.remove(os.path.join(cachefolder, filepath))
+
         return
-    cachefilename = os.path.join(cachefolder, arguments.get_cachename())
-    searchstruct = {}
 
-    if arguments.reset is False and os.path.exists(cachefilename):
-
-        searchstruct = pickle.load(open(cachefilename, "rb"))
-        print("\033[34mloading search",len(searchstruct),"\033[0m")
-    else:
-        print("\033[34mbuilding search\033[0m")
-
-        for root, folders, files in os.walk(arguments.searchfolder):
-            if len(searchstruct) % 100 == 0:
-                print_stdout(".")
-            for posfile in files:
-                possible_test_file_path = os.path.join(root, posfile)
-                #print(arguments.extension.strip(), possible_test_file_path)
-                if arguments.extension.strip()=="all" or possible_test_file_path.lower().endswith(arguments.extension.lower()):
-                    try:
-                        searchstruct[possible_test_file_path] = open(possible_test_file_path).read()
-                    except UnicodeDecodeError as ex:
-                        searchstruct[possible_test_file_path] = str(ex)
-                    except FileNotFoundError as ex:
-                        searchstruct[possible_test_file_path] = str(ex)
-                    except OSError as ex:
-                        searchstruct[possible_test_file_path] = str(ex)
-        pickle.dump(searchstruct, open(cachefilename, "wb"))
+    cachefilename=os.path.join(cachefolder, arguments.get_cachename())
+    searchstruct={}
+    searchstruct=dosearch(arguments, cachefilename, searchstruct)
 
     for filepath in searchstruct:
         if arguments.query.lower() in searchstruct[filepath].lower():
