d7d6a0335464dad77af7703ae00e25256f838aee
Erik de Jonge <erik@a8.nl>
2015-06-23 15:34:00 +0200

    Denis Leary: Ive been really excited about some new cutting edge electronic music and technology. Tuesday 23 June 2015 (week:25 day:174), 15:33:55

--- sortpyhist.py
+++ sortpyhist.py
@@ -12,7 +12,10 @@ author  : rabshakeh (erik@a8.nl)
 project : devenv
 created : 23-06-15 / 08:22
 """
+import os
 from arguments import Arguments
+from consoleprinter import console, console_warning
+
 class IArguments(Arguments):
     """
     IArguments
@@ -21,17 +24,23 @@ class IArguments(Arguments):
         """
         __init__
         """
-        self.help=False
-        self.input=""
+        self.help = False
+        self.input = ""
         super().__init__(doc)
+
 
 def main():
     """
     main
     """
     arguments = IArguments(__doc__)
-    print(arguments)
+    if os.path.exists(arguments.input):
 
-
+        cmdlist = open(arguments.input).read()
+        cmdlist = {cmd for cmd in cmdlist.split("\n") if len(cmd.strip())>0}
+        for cmd in cmdlist:
+            console(cmd, color=)
+    else:
+        console_warning(arguments.input, "file not found")
 if __name__ == "__main__":
     main()
