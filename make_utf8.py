ac9c2e4ad91f9b7d97d859581bb1e05c4492a975
Erik de Jonge <erik@a8.nl>
2015-07-13 15:38:18 +0200

    Penn Jillette: You arent wealthy until you have something money cant buy. Monday 13 July 2015 (week:28 day:194), 15:38:17

---
+++ make_utf8.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+# coding=utf-8
+"""
+Convert a file to plain ascii
+
+Usage:
+  make_utf8.py [options] <input>
+
+Options:
+  -h --help     Show this screen.
+
+author  : rabshakeh (erik@a8.nl)
+project : devenv
+created : 13-07-15 / 14:21
+"""
+import os
+
+from arguments import Arguments
+from consoleprinter import console
+
+
+class IArguments(Arguments):
+    """
+    IArguments
+    """
+    def __init__(self, doc):
+        """
+        __init__
+        """
+        self.help = False
+        self.input = ""
+        super().__init__(doc)
+
+
+def main():
+    """
+    main
+    """
+    arguments = IArguments(__doc__)
+    encoding = "utf-8"
+
+    if not os.path.exists("fileencoding.txt"):
+        console("no file [fileencoding.txt] found")
+        exit(1)
+    else:
+        encoding = open("fileencoding.txt").read().strip()
+
+    content = open(arguments.input, "rt", encoding=encoding).read()
+    content = content.encode("utf-8")
+    print(content)
+
+
+if __name__ == "__main__":
+    main()
