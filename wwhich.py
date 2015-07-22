636b7db001cc20d6e8045e25e510b07b48dc615a
Erik de Jonge <erik@a8.nl>
2015-07-22 15:47:41 +0200

    Wednesday 22 July 2015 (week:29 day:203), 15:47:40

--- wwhich.py
+++ wwhich.py
@@ -1,24 +1,29 @@
+#!/usr/bin/env python3
 # coding=utf-8
 """
-Lorum ipsum
+Follow all symbolic link paths, input <cmdpath> can be a command or a path
 
 Usage:
-  ${NAME} [options] <input>
+  wwhich.py [options] <cmdpath>
 
 Options:
   -h --help     Show this screen.
 
-author  : ${USER} (erik@a8.nl)
-project : ${PROJECT_NAME}
-created : ${DATE} / ${TIME}
+author  : rabshakeh (erik@a8.nl)
+project : devenv
+created : 22-07-15 / 15:20
 """
+
+import os
 import sys
 
+from sh import which
 from arguments import Arguments
 from consoleprinter import console
 
+
 if sys.version_info.major < 3:
-    console("Python 3 is required", color="red", plaintext="True")
+    print("\033[31mPython 3 is required\033[0m")
     exit(1)
 
 
@@ -31,7 +36,7 @@ class IArguments(Arguments):
         __init__
         """
         self.help = False
-        self.input = ""
+        self.cmdpath = ""
         super().__init__(doc)
 
 
@@ -40,8 +45,15 @@ def main():
     main
     """
     arguments = IArguments(__doc__)
-    console(arguments)
+    tracepath = ""
 
+    if os.path.exists(arguments.cmdpath):
+        tracepath = arguments.cmdpath
+    else:
+        tracepath = which(arguments.cmdpath)
+
+    console(tracepath)
 
 if __name__ == "__main__":
     main()
+
