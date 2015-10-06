68d01dd0c0fd606ecd91a485f8f00c910c714bfd
Erik de Jonge <erik@a8.nl>
2015-10-06 18:27:03 +0200

    Tuesday 06 October 2015 (week:40 day:279), 18:27:02

--- openfile.py
+++ openfile.py
@@ -43,7 +43,8 @@ def ossystem(cmd):
     @type cmd: str
     @return: None
     """
-    #print(cmd)
+
+    # print(cmd)
     os.system(cmd)
 
 
@@ -52,43 +53,50 @@ def main():
     main
     """
     arguments = IArguments(__doc__)
-    print(arguments.input)
 
+    print(arguments.input)
     arguments.input = os.path.abspath(os.path.expanduser(str(arguments.input)))
     alternative = ""
+
     if not os.path.exists(arguments.input):
         console(arguments.input, "not found", color="red")
-        alts = os.popen("mdfind "+os.path.basename(arguments.input)+" | grep "+os.path.basename(arguments.input).split(".")[0] +" | grep -v pyc").read()
+        alts = os.popen("mdfind " + os.path.basename(arguments.input) + " | grep " + os.path.basename(arguments.input).split(".")[0] + " | grep -v pyc").read()
         alts = [x.strip() for x in str(alts).split("\n") if x.strip()]
 
         if len(alts) > 0:
-            alts.sort(key=lambda x:len(x))
-            for alternative2 in alts:
+            alts.sort(key=lambda x: len(x))
 
-                if query_yes_no("Try \033[34m"+alternative2+"\033[96m instead?", default=False):
+            for alternative2 in alts:
+                if query_yes_no("Try \033[34m" + alternative2 + "\033[96m instead?", default=False):
                     alternative = alternative2
                     break
             else:
                 return
         else:
             return
+
     if alternative:
-        arguments.input =alternative
+        arguments.input = alternative
         console("opening", arguments.input, color="darkyellow")
+
     pycharm = False
+
     if arguments.input.lower().endswith("py"):
         pycharm = True
-
-    if os.path.isdir(arguments.input):
+    elif "http" in arguments.input:
+        os.system("osascript -e 'tell application \"Safari]\" to open location \"" + arguments.input + "\"")
+        os.system("osascript -e 'tell application \"Safari\" to activate';")
+        return
+    elif os.path.isdir(arguments.input):
         if os.path.exists(os.path.join(arguments.input, ".idea")):
             pycharm = True
+
         if os.path.exists(os.path.join(arguments.input, "setup.py")):
             pycharm = True
 
     if pycharm:
         ossystem("cd " + os.path.dirname(arguments.input) + "&&/Applications/PyCharm.app/Contents/MacOS/pycharm " + os.path.dirname(arguments.input) + " --line 1 " + arguments.input + " > /dev/null 2> /dev/null &")
         time.sleep(0.2)
-
         ossystem("osascript -e 'tell application \"Pycharm\" to activate'")
     else:
         os.system("cd " + os.path.dirname(arguments.input) + "&&/usr/bin/open " + arguments.input)
