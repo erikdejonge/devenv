e9f75c97233f34db7041db9be068b15961754b23
Erik de Jonge <erik@a8.nl>
2016-04-26 17:39:41 +0200

    Tuesday 26 April 2016 (week:17 day:117), 17:39:40

---
+++ new_python_file.py
@@ -0,0 +1,120 @@
+# coding=utf-8
+"""
+Project dev env
+
+Usage:
+  $new_python_file [options] <projectname> <filename>
+
+Options:
+  -h --help     Show this screen.
+
+author  : rabshakeh (erik@a8.nl)
+project : $dev env
+created : 26 Apr 2016 / 04:Apr
+where   :
+
+"""
+
+import sys
+
+
+from arguments import Arguments
+from consoleprinter import console
+
+if sys.version_info.major < 3:
+    console("Python 3 is required", color="red", plaintext="True")
+    exit(1)
+
+
+class IArguments(Arguments):
+    """
+    IArguments
+    """
+    def __init__(self, doc):
+        """
+        __init__
+        """
+        self.help = False
+        self.projectnaam = ""
+        self.filenaam = ""
+        super().__init__(doc)
+
+
+def main():
+    """
+    main
+    """
+
+    templ = """#!/usr/bin/env python3\n# coding=utf-8
+\"\"\"
+Project {PROJECT_NAME}
+
+Usage:
+  {NAME} [options] <input>
+
+Options:
+  -h --help     Show this screen.
+
+author  : {USER} (erik@a8.nl)
+project : {PROJECT_NAME}
+created : {DATE} / {TIME}
+where   : {WHEREAMI}
+\"\"\"
+import sys
+
+from arguments import Arguments
+from consoleprinter import console
+
+if sys.version_info.major < 3:
+    console(\"Python 3 is required\", color=\"red\", plaintext=\"True\")
+    exit(1)\n\n
+class IArguments(Arguments):
+    \"\"\"
+    IArguments
+    \"\"\"
+
+    def __init__(self, doc):
+        \"\"\"
+        __init__
+        \"\"\"
+        self.help = False
+        self.input = \"\"
+        super().__init__(doc)\n\n
+def main():
+    \"\"\"
+    main
+    \"\"\"
+    arguments = IArguments(__doc__)
+    console(arguments)\n\n
+if __name__ == \"__main__\":
+    main()
+"""
+    import time
+    from sh import whoami, whereami
+    arguments = IArguments(__doc__)
+
+    project_name = arguments.projectname
+    name = arguments.filename
+    user = whoami().strip()
+    whereamisplit = whereami().strip().split('\n')
+    if len(whereamisplit) > 0:
+        whereamisplit = whereamisplit[:2]
+        whereami = whereamisplit[0]+'\n'
+        whereami += '\n'.join([(10*' ')+x for x in whereamisplit[1:]]).lower()+"\n"
+        s = whereami.lower().replace("latitude", "").replace("longitude", "")
+        s = ','.join([x.strip() for x in s.lower().replace("latitude", "").replace("longitude", "").split(":") if x.strip()])
+        whereami += (10*' ')+"https://www.google.nl/maps/place/"+s
+    else:
+        whereami = "location not available"
+    localtime = time.localtime(time.time())
+    date = time.strftime("%d %b %Y", localtime)
+    time = time.strftime("%H:%M", localtime)
+    template_filled = templ.format(PROJECT_NAME=project_name, NAME=name, USER=user, DATE=date, TIME=time, WHEREAMI=whereami)
+
+    print(template_filled)
+
+
+if __name__ == "__main__":
+    main()
+
+
