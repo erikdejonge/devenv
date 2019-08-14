80844f9b156203abab84277169bcb085804b04ff
rabshakeh <erik@a8.nl>
2019-08-14 14:49:41 +0200

    -

--- reinstall_nvidia_bumblebee_arch.sh
+++ reinstall_nvidia_bumblebee_arch.sh
@@ -1,19 +1,21 @@
-yaourt -S core/mhwd-nvidia
-yaourt -S extra/libvdpau
-yaourt -S extra/libxnvctrl
-yaourt -S extra/linux51-nvidia
-yaourt -S extra/linux51-bbswitch
-yaourt -S extra/linux51-nvidia-340xx
-yaourt -S extra/linux51-nvidia-390xx
-yaourt -S extra/linux51-nvidia
-yaourt -S extra/nvidia-utils
-yaourt -S extra/opencl-nvidia
-yaourt -S bumblebee
-yaourt -S extra/linux51-bbswitch
-#yaourt -S community/cuda
-yaourt -S multilib/lib32-libvdpau
-yaourt -S multilib/lib32-nvidia-utils
-yaourt -S aur/nvdock-bumblebee
-yaourt -S aur/nvidia-bumblebee-applet
-yaourt -S aur/xgraphic
-yaourt -S aur/nvtop
+
+yay -S  --noconfirm core/mhwd-nvidia
+yay -S  --noconfirm extra/libvdpau
+yay -S  --noconfirm extra/libxnvctrl
+yay -S  --noconfirm extra/linux51-nvidia
+yay -S  --noconfirm extra/linux51-bbswitch
+#yay -S  --noconfirm extra/linux51-nvidia-340xx
+#yay -S  --noconfirm extra/linux51-nvidia-390xx
+yay -S  --noconfirm extra/linux51-nvidia
+yay -S  --noconfirm extra/nvidia-utils
+yay -S  --noconfirm extra/opencl-nvidia
+yay -S  --noconfirm bumblebee
+yay -S  --noconfirm extra/linux51-bbswitch
+yay -S  --noconfirm multilib/lib32-libvdpau
+yay -S  --noconfirm multilib/lib32-nvidia-utils
+yay -S  --noconfirm aur/nvdock-bumblebee
+yay -S  --noconfirm aur/nvidia-bumblebee-applet
+yay -S  --noconfirm aur/xgraphic
+yay -S  --noconfirm aur/nvtop
+yay -S  --noconfirm nvidia-xrun
+#yay -S  --noconfirm community/cuda
