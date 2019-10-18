db50ed8ab0d2838ada97cada54acc50d8fb134f1
Erik de Jonge <erik@a8.nl>
2019-10-18 13:06:14 +0200

    Friday 18 October 2019 (week:41 day:291), 13:06:13

---
+++ screenlayout/singlemonitor2.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+#xrandr --output HDMI1 --off --output DP1 --primary --mode 3840x2160 --pos 0x0 --rotate normal --output eDP1 --off --output VIRTUAL1 --off
+#!/bin/sh
+#xrandr --output HDMI1 --off --output DP1 --primary --mode 3840x2160 --pos 0x0 --rotate normal --output eDP1 --off --output VIRTUAL1 --off
+xrandr --output VIRTUAL1 --off --output eDP1 --off --output DP1 --primary --mode 3840x2160 --pos 0x0 --rotate normal --output HDMI1 --off --output DP1-3 --off --output DP1-2 --off --output DP1-1 --off
