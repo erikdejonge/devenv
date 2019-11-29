4734b3091e05da05b77c1f508a9cd652e78ea943
Erik de Jonge <erik@a8.nl>
2019-11-29 14:23:06 +0100

    -

--- myip.py
+++ myip.py
@@ -1,25 +1,21 @@
-#!/usr/bin/env python3
 # coding=utf-8
 """
-Project xx
+Project cryptocompare
 
 Usage:
-  xx [options] <input>
+  myip.py [options] <input>
 
 Options:
   -h --help     Show this screen.
 
 author  : rabshakeh (erik@a8.nl)
-project : xx
-created : 21 Jun 2018 / 16:33
-where   : 51.9568,4.582
-          https://www.google.nl/maps/place/51.9568,4.582
+project : cryptocompare
+created : 16-3-18 / 14:11
 """
 import sys
 
 from arguments import Arguments
 from consoleprinter import console
-
 if sys.version_info.major < 3:
     console("Python 3 is required", color="red", plaintext="True")
     exit(1)
@@ -29,7 +25,7 @@ class IArguments(Arguments):
     """
     IArguments
     """
-
+    
     def __init__(self, doc):
         """
         __init__
